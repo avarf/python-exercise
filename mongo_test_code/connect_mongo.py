60dcc1e7cf7677375d5c975c67cf2354b4a10445
avarf <varfanm@live.com>
2017-12-08 14:03:23 +0100

    working with mongodb added and also the query style and printing the json

--- mongo_test_code/connect_mongo.py
+++ mongo_test_code/connect_mongo.py
@@ -1,5 +1,10 @@
-# from pymongo import Connection
+import pymongo
 from pymongo import MongoClient
+import json
+import collections
+from bson import json_util
+from pprint import pprint
+
 
 """
 class Mongo_Work(object):
@@ -25,52 +30,166 @@ class Mongo_Work(object):
 
 
 
+rec1 = {
+	'robotID':'r1',
+	'pose':'5,8',
+	'busy':'y',
+	'battery':'51',
+	'sensors':[
+	{'IR':'1'},
+	{'sonar':'0'},
+	{'laser':'1'},
+	{'microphone':'0'}
+	],
+	'motor values':[
+	{
+		'motro1':'8',
+		'motro2':'9',
+		'motro3':'9',
+		'motro4':'8'
+	}
+	]
+}
 
-if __name__ == "__main__":
-	print('Inside the __main__')
+rec2 = {
+	'robotID':'r2',
+	'pose':'0,0',
+	'busy':'n',
+	'battery':'85',
+	'sensors':[
+	{'IR':'1'},
+	{'sonar':'1'},
+	{'laser':'1'},
+	{'microphone':'0'}
+	],
+	'motor values':[
+	{
+		'motro1':'7',
+		'motro2':'5',
+		'motro3':'5',
+		'motro4':'8'
+	}
+	]
+}
 
-	# con = Connection()
-	con = MongoClient()
-	# creating database
-	db = con.test_db
-	# creating dataset
-	# collection = db.ds_test
+rec3 = {
+	'robotID':'r3',
+	'pose':'5,15',
+	'busy':'y',
+	'battery':'45',
+	'sensors':[
+	{'IR':'1'},
+	{'sonar':'1'},
+	{'laser':'1'},
+	{'microphone':'0'}
+	],
+	'motor values':[
+	{
+		'motro1':'9',
+		'motro2':'8',
+		'motro3':'9',
+		'motro4':'7'
+	}
+	]
+}
 
-	# members is a collection
-	# result = db.members.insert_one({
-	# 	'name': 'Ali',
-	# 	'family':'varfan',
-	# 	'sex':'male',
-	# 	'mem_id': '1'
-	# 	})
-	# result2 = db.members.insert_one({
-	# 	'name': 'Selale',
-	# 	'family':'Ozbay',
-	# 	'sex':'female',
-	# 	'id':'2'
-	# 	})
-	# print(result2)
-	docs = db.members.find()
-	for d in docs:
-		print(docs)
+rec4 = {
+	'robotID':'r4',
+	'pose':'10,6',
+	'busy':'y',
+	'battery':'88',
+	'sensors':[
+	{'IR':'1'},
+	{'sonar':'0'},
+	{'laser':'1'},
+	{'microphone':'0'}
+	],
+	'motor values':[
+	{
+		'motro1':'5',
+		'motro2':'9',
+		'motro3':'9',
+		'motro4':'6'
+	}
+	]
+}
+
+rec5 = {
+	'robotID':'',
+	'pose':'',
+	'busy':'',
+	'battery':'',
+	'sensors':[
+	{'IR':'1'},
+	{'sonar':'0'},
+	{'laser':'1'},
+	{'microphone':'0'}
+	],
+	'motor values':[
+	{'motro1':''},
+	{'motro2':''},
+	{'motro3':''},
+	{'motro4':''}
+	]
+}
 
 
-	# db.insert({
-	# 	'name': 'Ali',
-	# 	'family':'varfan',
-	# 	'sex':'male'
-	# 	})
+# another way to work with mongodb
+# database["test"].insert({'name': 'foo'})
+#     doc = database["test"].find_one({'name': 'foo'})
+#     return json.dumps(doc, sort_keys=True, indent=4, default=json_util.default)
 
-	# db.insert({
-	# 	'name': 'Selale',
-	# 	'family':'Ozbay',
-	# 	'sex':'female'
-	# 	})
+con = MongoClient()
+# creating database
+db = con.test_db
 
-	# records = db.find()
+# members is a collection
+# result = db.members.insert_one(rec1)
+# result = db.members.insert_one(rec2)
+# result = db.members.insert_one(rec3)
+# result = db.members.insert_one(rec4)
 
-	# print('Records in db:')
-	# for rec in records:
-	# 	print(rec)
 
-	print('Finished')
+# q1 = db.members.find({'busy':'n'})
+# for q in q1:
+# 	pprint(q)
+
+# result = db.members.delete_many({'busy':'y'})
+docs = db.members.find()
+# for d in docs:
+# 	# jd = json.dumps(d)
+# 	jd = json.dumps(d, sort_keys=True, indent=4, default=json_util.default)
+# 	# jd = json.loads(d)
+# 	print(jd)
+
+jd = json.dumps(docs[0], sort_keys=True, indent=4, default=json_util.default)
+jd = json.loads(jd)
+
+pprint(jd)
+print('--------------')
+# jd = json.loads(jd)
+
+print(jd['battery'])
+print('--------------')
+
+print(jd['motor values'][0]['motro2'])
+print('--------------')
+
+
+# for d in docs:
+# 	pprint(d)
+
+print('Finished')
+
+
+# TO DO:
+# THE SOLUTION: we have to create a list of one dict and work with it as follow:
+# jd['motor values'][0]['motro2']
+# definition:
+# 'motor values':[{
+# 	'motro2':'9',
+# 	'motro2':'5'
+# }]
+# I have to define the records as dict of dict instead of list of dict because of having access to them
+# in dict of dict we can write: jd['motor values']['motro2']
+# but in list of dict we have to write: jd['motor values'][1]
